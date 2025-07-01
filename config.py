e662981cdfb8b94e2de660abb8e2dd8b46fc429f
lilythsan <neha226v@gmail.com>
2025-07-01 08:07:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")  
 
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
-from dotenv import load_dotenv
-load_dotenv()
+
 
