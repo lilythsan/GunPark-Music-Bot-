cae7fde7f801cbc1da1062cb9b54b7d486c9f6ee
miss lilyth sparrow <neha226v@gmail.com>
2025-07-04 21:44:25 +0530

    Create __init__.py

---
+++ pluging/__init__.py
@@ -0,0 +1,15 @@
+# plugins/__init__.py
+
+"""
+GunPark Plugin System
+---------------------
+Yeh folder saare plugins ko load karta hai:
+- Web Games
+- AI Chatbot
+- Music System
+- Downloader (YT/Insta/PDF)
+- Weather
+- Translator
+"""
+
+print("✅ plugins/ package loaded successfully.")
