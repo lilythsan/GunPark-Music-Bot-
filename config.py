7c1546d172ba31b5489f91ada70436b58af29feb
miss lilyth sparrow <neha226v@gmail.com>
2025-07-04 20:19:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
-
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
 
