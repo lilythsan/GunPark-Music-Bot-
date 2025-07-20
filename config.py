5f7b2f04d3199c9eec483663de5ec0c98635131f
miss lilyth sparrow <neha226v@gmail.com>
2025-07-20 11:08:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-# .env file load karo (isse private credentials fetch honge)
+# .env file load karo
 load_dotenv(dotenv_path=".env")
 
 # Bot Configurations
@@ -10,10 +10,10 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
 
-# MongoDB URI
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
+# MongoDB URI (corrected key name)
+MONGO_DB_URI = os.getenv("MONGO_URL", "")
 
 # Assistant VC Account Details
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION")
-ASSISTANT_API_ID = int(os.getenv("ASSISTANT_API_ID"))
-ASSISTANT_API_HASH = os.getenv("ASSISTANT_API_HASH")
+ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "")
+ASSISTANT_API_ID = int(os.getenv("ASSISTANT_API_ID", 0))
+ASSISTANT_API_HASH = os.getenv("ASSISTANT_API_HASH", "")
